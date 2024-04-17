1a5b93bda5d14c421a1d7a8cb236518fcd1eaf62
Xinghai.Liu <xinghai.liu@ibaiqiu.com>
2024-04-17 18:53:51 +0800

    feat:update

--- components.d.ts
+++ components.d.ts
@@ -11,5 +11,6 @@ declare module 'vue' {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Test: typeof import('./src/components/Test.vue')['default']
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,8 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <Test msg="Vite + Vue" />
 </template>
-
+<script setup lang="ts">
+import Test from "./components/Test.vue";
+</script>
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
